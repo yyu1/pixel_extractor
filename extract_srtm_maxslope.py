d4433bb73ab32906796842a4e570e35cef73a165
Yifan Yu <koensayr@gmail.com>
2017-01-12 15:52:13 -0800

    properly output output, and also return value from memmap function

--- extract_srtm_maxslope.py
+++ extract_srtm_maxslope.py
@@ -4,6 +4,8 @@ import memmap_extraction as me
 infile = '/nobackupp6/nexprojects/CMS-ALOS/srtm1sec_mosaic/global_srtm3_aster_maxdegreeslope_3sec.byt'
 csvfile = '/nobackupp6/nexprojects/CMS-ALOS/glas/global_glas_2003_2008_alltypes_formatted_agb_v6_srtmstdev.csv'
 outfile = '/nobackupp6/nexprojects/CMS-ALOS/glas/global_glas_2003_2008_alltypes_formatted_agb_v6_srtmstdev_maxslope.csv'
+#csvfile = '/nobackupp6/nexprojects/CMS-ALOS/glas/test.csv'
+#outfile = '/nobackupp6/nexprojects/CMS-ALOS/glas/test_output.csv'
 
 datatype = np.uint8
 
@@ -25,9 +27,9 @@ extraction_values = me.memmap_extraction(infile, datatype, xdim, ydim, rows, col
 with open(outfile, 'w') as output:
 	with open(csvfile) as input:
 		header = next(input)
-		output.write(header+',\'srtm_maxslope\'')
+		output.write(header[:-1]+',\'srtm_maxslope\'\n')
 
 		index = 0
 		for line in input:
-			output.write(line+','+str(extraction_values[index]))
+			output.write(line[:-1]+','+str(extraction_values[index])+'\n')
 			index = index + 1
